3273861cbde53760543b35857513227c4ef68d88
mike-suggitt <mikessuggitt@gmail.com>
2019-02-25 21:56:44 +0000

    - Remove lib dir  - Add package-lock  - Add node-sass devDep  - Update loader

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -1,132 +1,99 @@
-const path = require('path');
-const fs = require('fs');
-const argv = require('yargs').argv;
+var path = require('path');
+var fs = require('fs');
+var argv = require('yargs').argv;
 
-const jobsArray = [];
-const configTags = argv.configTags || [];
+var configTags = argv.configTags || [];
 
-//Get application configs
-function getConfigs(_base, _dir) {
-    var _path = _base + "/" + _dir;
-    var list = fs.readdirSync(_path);
-    for(var i=0;i<list.length;i++) {
-        var file = list[i];
-
-        if(file[0] !== ".") {
-            if(fs.lstatSync(_path + "/" + file).isDirectory()) {
-                getConfigs(_path, file)
+function fetchConfig(basePath, dir) {
+    const files = fs.readdirSync(`${basePath}/${dir}`);
+    let configs = [];
+    files.forEach(function (file) {
+        if (file[0] !== '.') {
+            if (fs.lstatSync(`${basePath}/${dir}/${file}`).isDirectory()) {
+                configs = configs.concat(fetchConfig(`${basePath}/${dir}`, file));
             }
             else {
-                var config = JSON.parse(fs.readFileSync(path.resolve(__dirname, _path + "/" + file)).toString());
-                if(configTags.length > 0 && configTags.indexOf(config.tag) < 0) {
-                    continue;
-                }
-                if(!config.exclude) {
-                    var id = _dir + "/" + file;
-
-                    var jobEnv = "";
-                    if(config.jobs.env) {
-                        jobEnv = config.jobs.env;
-                        delete config.jobs.env;
-                    }
-
-                    for(var jobType in config.jobs) {
-                        for(var jobId in config.jobs[jobType]) {
-                            var newJobId = id + "_" + jobId;
-                            if(!jobsArray[jobType]) {
-                                jobsArray[jobType] = {};
-                            }
-                            jobsArray[jobType][newJobId] = config.jobs[jobType][jobId]
-                            jobsArray[jobType][newJobId].id = jobId;
-                            if(!jobsArray[jobType][newJobId].env && jobEnv) {
-                                jobsArray[jobType][newJobId].env = jobEnv;
-                            }
-                            delete config.jobs[jobType][jobId];
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-function includeAfterTagFilter() {
-
-}
-
-
-
-function buildConfig(basePath, dir) {
-    const target = `${basePath}/${dir}`;
-    const list = fs.readdirSync(target);
-    list.forEach(file => {
-        if(file[0] !== '.') {
-            if(fs.lstatSync(`${target}/${file}`).isDirectory()) {
-                buildConfig(target, file)
-            }
-            else {
-                const config = require(path.resolve(__dirname, `${target}/${file}`));
-                // if(configTags.length > 0 && configTags.indexOf(config.tag) < 0) {
-                //     continue;
-                // }
-                if(!config.exclude) {
-                    const id = `${dir}/${file}`;
-
-                    let jobEnv = "";
-                    if(config.jobs.env) {
-                        jobEnv = config.jobs.env;
-                        delete config.jobs.env;
-                    }
-
-                    for(var jobType in config.jobs) {
-                        for(var jobId in config.jobs[jobType]) {
-                            var newJobId = id + "_" + jobId;
-                            if(!jobsArray[jobType]) {
-                                jobsArray[jobType] = {};
-                            }
-                            jobsArray[jobType][newJobId] = config.jobs[jobType][jobId];
-                            jobsArray[jobType][newJobId].id = jobId;
-                            if(!jobsArray[jobType][newJobId].env && jobEnv) {
-                                jobsArray[jobType][newJobId].env = jobEnv;
-                            }
-                            delete config.jobs[jobType][jobId];
-                        }
-                    }
-                }
+                configs.push(`${basePath}/${dir}/${file}`);
             }
         }
     });
+    return configs;
+}
+
+function fetchAllConfigs(basePath, dir) {
+    const allConfigs = fetchConfig(basePath, dir);
+    return allConfigs.map(config => {
+        const configObj = require(config);
+        return {
+            id: config,
+            ...configObj
+        };
+    }).filter(config => !config.exclude)
+}
+
+
+function buildJobs(basePath, dir) {
+    const configs = fetchAllConfigs(basePath, dir);
+    const jobsArray = {};
+
+    configs.forEach(config => {
+        let jobEnv;
+        if (config.jobTypes.env) {
+            jobEnv = config.env;
+            delete config.env;
+        }
+        for (const jobTypeId in config.jobTypes) {
+            const jobType = config.jobTypes[jobTypeId];
+            const jobTypeParams = jobType.params || {};
+            for (const jobId in jobType.jobs) {
+                if (!jobsArray[jobTypeId]) {
+                    jobsArray[jobTypeId] = {};
+                }
+                const newJobId = config.id + "_" + jobId;
+                const job = {
+                    ...jobType.jobs[jobId],
+                    ...jobTypeParams
+                };
+                job.id = jobId;
+                if (!job.env && jobEnv) {
+                    job.env = jobEnv;
+                }
+                jobsArray[jobTypeId][newJobId] = job;
+                delete config.jobTypes[jobTypeId][jobId];
+            }
+        }
+    });
+
+    return jobsArray;
+
 }
 
 function load(configPath, jobDirPath) {
-    buildConfig(configPath, '');
-    runJobs(jobDirPath);
+    const jobs = buildJobs(configPath, '');
+    runJobs(jobDirPath, jobs);
 }
 
-function runJobs(jobDirPath) {
-    for(var jobType in jobsArray) {
+function runJobs(jobDirPath, jobsArray) {
+    for (var jobType in jobsArray) {
         try {
-            const jobPath = path.resolve(jobDirPath, jobType.toLowerCase() + ".js");
+            var jobPath = path.resolve(jobDirPath, jobType.toLowerCase() + ".js");
 
             var jobClass = require(jobPath);
             var jobGroup = jobsArray[jobType];
-            for(var id in jobGroup) {
-
-                if(typeof(jobGroup[id].env) === "undefined") {
+            for (var id in jobGroup) {
+                if (typeof jobGroup[id].env === "undefined") {
                     jobGroup[id].env = "live";
                 }
+
                 var job = new jobClass(jobGroup[id]);
 
                 job.start();
-
             }
-        }catch(e) {
+        } catch (e) {
             console.log(e);
         }
     }
 }
-
-
 
 module.exports = {
     load: load
